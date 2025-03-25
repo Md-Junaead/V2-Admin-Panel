f310786d6b433668c93aecc68f340e37ed64c705
Md. Junaead <junaead12364@gmail.com>
2025-03-25 14:08:28 +0600

    File Structure changed

--- lib/configs/routes/routes.dart
+++ lib/configs/routes/routes.dart
@@ -1,10 +1,10 @@
 import 'package:admin_panel/configs/routes/routes_name.dart';
 import 'package:admin_panel/screens/dashboard/all_user/reg_user_screen.dart';
 import 'package:admin_panel/screens/dashboard/active_user/all_user.dart';
-import 'package:admin_panel/screens/dashboard/bank_withdraw.dart';
-import 'package:admin_panel/screens/dashboard/crypto_withdraw.dart';
-import 'package:admin_panel/screens/dashboard/emi.dart';
-import 'package:admin_panel/screens/dashboard/inactive_user.dart';
+import 'package:admin_panel/screens/dashboard/withdraw/bank_withdraw.dart';
+import 'package:admin_panel/screens/dashboard/withdraw/crypto_withdraw.dart';
+import 'package:admin_panel/screens/dashboard/loan/emi.dart';
+import 'package:admin_panel/screens/dashboard/inactive_user/inactive_user.dart';
 import 'package:admin_panel/screens/dashboard/loan/loan_screen.dart';
 import 'package:admin_panel/screens/deposit/deposit_screen.dart';
 import 'package:admin_panel/view/dashboard_view.dart';
--- lib/view/dashboard_view.dart
+++ lib/view/dashboard_view.dart
@@ -1,10 +1,10 @@
 // dashboard_view.dart (updated)
 import 'package:admin_panel/screens/dashboard/all_user/reg_user_screen.dart';
 import 'package:admin_panel/screens/dashboard/active_user/all_user.dart';
-import 'package:admin_panel/screens/dashboard/bank_withdraw.dart';
-import 'package:admin_panel/screens/dashboard/crypto_withdraw.dart';
-import 'package:admin_panel/screens/dashboard/emi.dart';
-import 'package:admin_panel/screens/dashboard/inactive_user.dart';
+import 'package:admin_panel/screens/dashboard/withdraw/bank_withdraw.dart';
+import 'package:admin_panel/screens/dashboard/withdraw/crypto_withdraw.dart';
+import 'package:admin_panel/screens/dashboard/loan/emi.dart';
+import 'package:admin_panel/screens/dashboard/inactive_user/inactive_user.dart';
 import 'package:admin_panel/screens/dashboard/loan/loan_screen.dart';
 import 'package:admin_panel/screens/deposit/deposit_screen.dart';
 import 'package:admin_panel/view/sidebar_view.dart';
